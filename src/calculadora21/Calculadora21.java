c8fbb6664343fd87b0f8112c04245cc65e5a88b9
Aritz Torrez <aritz.torres06@somo.eus>
2026-02-10 13:35:00 +0100

    Añadidos comentarios de Javadoc

--- src/calculadora21/Calculadora21.java
+++ src/calculadora21/Calculadora21.java
@@ -1,20 +1,43 @@
 package calculadora21;
 
+
+/**
+ * * @author Aritz
+ */
 public class Calculadora21 {
+	/**
+     * Suma dos números enteros.
+     */
 	public int sumar(int a, int b) { 
 		return a + b; 
 	} 
+	
+	/**
+     * Resta dos números enteros.
+     */
 	public int restar(int a, int b) { 
 		 return a - b; 
 	}
+	
+	/**
+     * Multiplica dos números enteros.
+     */
 	public int multiplicar(int a, int b) { 
 		 return a * b; 
 	} 
+	
+	/**
+     * Divide dos números enteros.
+     */
 	public double dividir(int a, int b) { 
 		 if (b == 0) { 
 			 throw new IllegalArgumentException("No se puede dividir entre cero");  } 
 		 return (double) a / b; 
 	}
+	
+	/**
+     * Calcula la potencia de una base elevada a un exponente.
+     */
 	public int potencia(int base, int exponente) { 
 		 int resultado = 1; 
 		 for (int i = 0; i < exponente; i++) { 
@@ -22,6 +45,10 @@ public class Calculadora21 {
 		 } 
 		 return resultado; 
 	} 
+	
+	/**
+     * Calcula el resto de la división entre dos números.
+     */
 	public int modulo(int a, int b) { 
 		 return a % b; 
 	} 
